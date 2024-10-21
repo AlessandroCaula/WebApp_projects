f19e742f21664bf04f0db0367244bda3f3a461de
Alessandro Caula <alecolin96@gmail.com>
2024-10-21 18:05:18 +0200

    New Fit-App developments.

---
+++ react-js_fit-app/src/components/Generator.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Generator() {
+  return (
+    <div>From the Generator component</div>
+  )
+}
---
+++ react-js_fit-app/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// The advantage of JSX is that it allows to have JavaScript written inside the HTML code. 
+// Funciton Component. It always have the same name of the file. It always start with a capital letter. 
+export default function Hero() {
+  return (    
+    <div>From the Hero component</div>
+  )
+}
