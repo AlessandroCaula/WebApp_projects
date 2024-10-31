c6d8e99d40ffee59d333a26300f913c07e4a4c7c
Alessandro Caula <alecolin96@gmail.com>
2024-10-31 23:36:35 +0100

    Small changes on the new project

--- react-js_free-scribe/src/App.jsx
+++ react-js_free-scribe/src/App.jsx
@@ -2,8 +2,20 @@
 function App() {
 
   return (
-    <div>
+    <div className="flex flex-col p-4">
+      {/*"min-h-screen" will apply the background color defined in the index.html to the entier page*/}
+      <section className="min-h-screen flex flex-col">
+        <header>
+          <h1>FreeScribe</h1>
+        </header>
+        <main className="flex-1">
+
+        </main>
+      </section>
       <h1 className="text-green-400">TEST</h1>
+      <footer>
+
+      </footer>
     </div>
   )
 }
