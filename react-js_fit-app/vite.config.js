925db6db0e664e826bed0446568f6fa699b8706c
Alessandro Caula <alecolin96@gmail.com>
2024-10-31 20:28:16 +0100

    Small fix

--- react-js_fit-app/vite.config.js
+++ react-js_fit-app/vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   sourcemap: true, // Ensure source maps are enabled
-  // },
-  // server: {
-  //   port: 3000, // Customize the port if needed
-  // },
-  // // Force explicit source map generation in development mode
-  // esbuild: {
-  //   sourcemap: true,
-  // }
 })
