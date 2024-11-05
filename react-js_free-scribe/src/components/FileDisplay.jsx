af15c8d4e69c26923edf6e707d0c7ec22a82d015
Alessandro Caula <alecolin96@gmail.com>
2024-11-05 23:27:52 +0100

    New Start Recording function.

--- react-js_free-scribe/src/components/FileDisplay.jsx
+++ react-js_free-scribe/src/components/FileDisplay.jsx
@@ -5,15 +5,21 @@ export default function FileDisplay(props) {
     const {file, audioStream, handleAudioReset} = props;
 
     return (
-        <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20">
+        <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20 w-fit max-w-full mx-auto">
             <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>Your <span className='text-blue-400 bold'>File</span></h1>
-            <div className='mx-auto flex flex-col text-left'>
+            <div className='mx-auto flex flex-col text-left my-4'>
                 <h3 className='font-semibold'>Name</h3>
                 <p>{file.name}</p>
             </div>
             <div className='flex items-center justify-between gap-4'>
-                <button className='text-slate-400 '>Reset</button>
-                <button className='specialBtn'></button>
+                <button onClick={handleAudioReset} className='text-slate-400 hover:text-blue-600'>Reset</button>
+                <button className='specialBtn px-3 p-2 rounded-lg text-blue-400 flex items-center gap-2 font-medium'>
+                    {/*In order to add an icon (from Fontawesome) in the button, near the Transcribe text, put the text in a paragraph <p></p>*/}
+                    <p>
+                        Transcribe
+                    </p>
+                    <i className="fa-solid fa-pen-nib"></i>
+                </button>
             </div>
         </main>
     )
--- react-js_free-scribe/src/components/Homepage.jsx
+++ react-js_free-scribe/src/components/Homepage.jsx
@@ -1,31 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Homepage(props) {
   // Deconstruct the props passed to the Homepage.
-  const {setFile, setAudioStream} = props;
-  
+  const { setFile, setAudioStream } = props;
+
+  // Create useState variables.
+  const [recordingStatus, setRecordingStatus] = useState('inactive');
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+
+  // The useRef hook in React provides a way to hold and persist mutable values across re-renders without causing additional re-renders when the reference value changes. It's commonly used to directly access and manupulate DOM elements, store values that don't need to trigger re-renders, and create a stable reference that persists over the component's lifecycle.
+  const mediaRecorder = useRef(null);
+
+  // The MIME types specify the nature and format of a file, especially when it comes to media or downloadable content. The MIME type 'audio/webm' represents audio data in WebM format. 
+  const mimeType = 'audio/webm';
+
+  // !! When you define a function as aync, it automatically returns a Promise. Inside this function, you can use the await keyword to pause the function's execution until a given promise resolves or reject, making the code appear as it it's running sequentially. 
+  // !! An async function does't block the main thread (including rendering) while it waits for asynchronous operations, like fetching data. When you use await inside an async function, the function pauses only in its own execution context to wait for the Promise to resolve, without stopping other code outside of it from running. Even though the async function itself might "pause", this does not affect the overall page rendering or order active tasks on the main thread. 
+  // - Non-blocking rendering: The rest of the application can keep rendering or handling user interactions while the async function completes in the background.
+  // - Smoother performace: Since the main thread isn't held up, the application remains responsive, enhancing the user experience. 
+  async function startRecording() {
+    let tempStream;
+    console.log('Start recording');
+
+    // Try and catch block to catch any exception. 
+    try {
+      // Allow the web application to access a user's media devices, such as the camera and the microphone in this case. In this case we want to access only the audio, not the video. 
+      const streamData = navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+      tempStream = streamData;
+    } catch (err) {
+      console.log(err.message);
+      return;
+    }
+
+    // Create new Media recorder instance using the stream. 
+    // The MediaRecorder interface is used in JS to record media streams, such as audio and video, that have been obtained through navigator.mediaDevices.getUserMedia() or similar APIs. 
+    // mimeType: Specifies the format of the media being recorded.
+    // By creating an instance of MediaRecorder, you can control the recording process, start and stop it, and handle recorded data. 
+    const media = new MediaRecorder(tempStream, { type: mimeType });
+    // Is usually part of React when managing a MediaRecorder instance using a useRef hook. Here's a breakdown of how and why this is used. 
+    // Using useRef allows you to store a reference to the MediaRecorder instance that persists across renders without triggering re-renders. 
+    // In React, useRef is commonly used for mutable values that shouldn't trigger re-renders. A MediaRecorder instance can change its state (start, pause, stop recording) without the need to re-reder the component. Storing it in a ref lets you control it directly without causing unnecessary updates in the component's state.
+    mediaRecorder.current = media;
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      // Handle the case in which there is no data. 
+      if (typeof event.data === 'undefined') { return; }
+      if (typeof event.data.size === 0) { return; }
+      // Pushing the data to the localAudioChunks. 
+      localAudioChunks.push(event.data);
+    }
+
+    // Setting the Audio Chunks State. 
+    setAudioChunks(localAudioChunks);
+  }
+
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center pb-20">
-        <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
-        <h3 className='font-medium md:text-lg'>Record <span 
+      <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
+      <h3 className='font-medium md:text-lg'>Record <span
         className='text-blue-400'>&rarr;</span> Transcribe <span
-        className='text-blue-400'>&rarr;</span> Translate</h3>
-        <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-lg'>
-            <p className='text-blue-400'>Record</p>
-            <i className="fa-solid fa-microphone"></i>
-        </button>
-        {/*Or upload your file. It can only accept mp3 or wave recording files*/}
-        {/*- <input> element: which is often used for taking user input, such as text or file uploads.
+          className='text-blue-400'>&rarr;</span> Translate</h3>
+      <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-lg'>
+        <p className='text-blue-400'>Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+      {/*Or upload your file. It can only accept mp3 or wave recording files*/}
+      {/*- <input> element: which is often used for taking user input, such as text or file uploads.
         - 'hidden' class; is typically used to make an element invisible on the page.
         - type='file': this attribute changes the input into a file upload field, allowing users to select files from their device when the input is shown.*/}
-        {/*In the input class we are gonna set the onChange event to handle the file upload and set the setFile useState variable.*/}
-        <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload <input onChange={(e) => {
-          // Retrieve the selected file.
-          const tempfile = e.target.files[0];
-          // Set the file to the setFile useState variable.
-          setFile(tempfile);
-        }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
-        <p className='italic text-slate-500'>Free now Free forever</p>
+      {/*In the input class we are gonna set the onChange event to handle the file upload and set the setFile useState variable.*/}
+      <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload <input onChange={(e) => {
+        // Retrieve the selected file.
+        const tempfile = e.target.files[0];
+        // Set the file to the setFile useState variable.
+        setFile(tempfile);
+      }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
+      <p className='italic text-slate-400'>Free now free forever</p>
     </main>
   )
 }
