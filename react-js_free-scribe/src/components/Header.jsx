d8894aa473777ab664b6352b218367971a4b1341
Alessandro Caula <alecolin96@gmail.com>
2024-11-08 23:31:29 +0100

    Small fix for the FileDisplay layout page.

--- react-js_free-scribe/src/components/Header.jsx
+++ react-js_free-scribe/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <header className="flex items-center justify-between gap-4 p-4">
             <h1 className='font-semibold'>Free<span className="text-blue-400 bold">Scribe</span></h1>
             {/*Givint the specialBtn class for styling this button*/}
-            <button className="flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400">
+            <button className="flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400">
                 <p>New</p>
                 <i className="fa-solid fa-plus"></i>
             </button>
