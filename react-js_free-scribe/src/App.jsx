2e6ab7fc5bc1b5ecdd384cbbc7cc80944c20b829
Alessandro Caula <alecolin96@gmail.com>
2024-11-09 00:01:31 +0100

    Transcribing component, and final page logic.

--- react-js_free-scribe/src/App.jsx
+++ react-js_free-scribe/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import FileDisplay from "./components/FileDisplay";
 import HomePage from "./components/Homepage";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
 
 function App() {
   // useState variables
@@ -9,9 +11,13 @@ function App() {
   const [file, setFile] = useState(null);
   // The audioStream exist when we click record button and have a recorderd audio.
   const [audioStream, setAudioStream] = useState(null);
+  // The transcribed useState Hook variable final output
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   // Boolean check to see if there exist an audio available, both file or audioStream file.
   const isAudioAvailable = file || audioStream;
-  
+
   // Define a function to reset the audio. Set the files and the audioStream state variables to null. 
   function handleAudioReset() {
     setFile(null);
@@ -29,9 +35,14 @@ function App() {
       <section className="min-h-screen flex flex-col">
         {/*Rendering out the Header*/}
         <Header />
-        {/*Rendering out the Home page*/}
-        {/*Add some conditional logic. If there exists any type of audio (file or audioStream) we are gonna rendere out the FileDisplay component. Otherwise we are gonna render out the Homepage component*/}
-        {isAudioAvailable ? (
+        {/*Conditionally render when the output exists. The we render out the Information component. This is the first thing ot chek in the rendering of the page. Otherwise, we end up rendering the other component, even if the final output exist. That's why we check it first*/}
+        {/*If it does not exist we are going to check if loading exist. If it exists, we are going to render out Transcribing.*/}
+        {/*If also the Transcribing does not exist, we are going to finally renering the home page.*/}
+        {output ? (
+          <Information0 />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
           // Passing down the file or the audioStream file as well as the handleAudioReset function.
           <FileDisplay file={file} audioStream={audioStream} handleAudioReset={handleAudioReset} />
         ) : (
