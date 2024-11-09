d25196061aea1f5fd7177175cc2ef11cecc89732
Alessandro Caula <alecolin96@gmail.com>
2024-11-09 15:31:58 +0100

    Implementation of the Transcription and Translation components.

--- react-js_free-scribe/src/App.jsx
+++ react-js_free-scribe/src/App.jsx
@@ -6,56 +6,54 @@ import Information from "./components/Information";
 import Transcribing from "./components/Transcribing";
 
 function App() {
-  // useState variables
-  // The file state exist when we upload a file.
-  const [file, setFile] = useState(null);
-  // The audioStream exist when we click record button and have a recorderd audio.
-  const [audioStream, setAudioStream] = useState(null);
-  // The transcribed useState Hook variable final output
-  const [output, setOutput] = useState(null);
-  const [loading, setLoading] = useState(false);
+    // useState variables
+    // The file state exist when we upload a file.
+    const [file, setFile] = useState(null);
+    // The audioStream exist when we click record button and have a recorderd audio.
+    const [audioStream, setAudioStream] = useState(null);
+    // The transcribed useState Hook variable final output
+    const [output, setOutput] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-  // Boolean check to see if there exist an audio available, both file or audioStream file.
-  const isAudioAvailable = file || audioStream;
+    // Boolean check to see if there exist an audio available, both file or audioStream file.
+    const isAudioAvailable = file || audioStream;
 
-  // Define a function to reset the audio. Set the files and the audioStream state variables to null. 
-  function handleAudioReset() {
-    setFile(null);
-    setAudioStream(null);
-  }
+    // Define a function to reset the audio. Set the files and the audioStream state variables to null. 
+    function handleAudioReset() {
+        setFile(null);
+        setAudioStream(null);
+    }
 
-  // Since the audioStream is not updated when start and stop recording are executed in the HomePage, and the FileDisplayed is not rendered (since the audioStream is not updated) we will use a useEffect Hook.
-  useEffect(() => {
-    console.log(audioStream);
-  }, [audioStream])
+    // Since the audioStream is not updated when start and stop recording are executed in the HomePage, and the FileDisplayed is not rendered (since the audioStream is not updated) we will use a useEffect Hook.
+    useEffect(() => {
+        console.log(audioStream);
+    }, [audioStream])
 
-  return (
-    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
-      {/*"min-h-screen" will apply the background color defined in the index.html to the entier page*/}
-      <section className="min-h-screen flex flex-col">
-        {/*Rendering out the Header*/}
-        <Header />
-        {/*Logic for the component rendering of the page (which one to show based on what data is available*/}
-        {/*Conditionally render when the output exists. The we render out the Information component. This is the first thing ot chek in the rendering of the page. Otherwise, we end up rendering the other component, even if the final output exist. That's why we check it first*/}
-        {/*If it does not exist we are going to check if loading exist. If it exists, we are going to render out Transcribing.*/}
-        {/*If also the Transcribing does not exist, we are going to finally renering the FileDisplay or the HomePage.*/}
-        {output ? (
-          <Information0 />
-        ) : loading ? (
-          <Transcribing />
-        ) : isAudioAvailable ? (
-          // Passing down the file or the audioStream file as well as the handleAudioReset function.
-          <FileDisplay file={file} audioStream={audioStream} handleAudioReset={handleAudioReset} />
-        ) : (
-          // We are gonna pass the setFile and setAudioStream State variables down the Homepage component as a props.
-          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
-        )}
-      </section>
-      <h1 className="text-green-400">TEST</h1>
-      <footer>
-      </footer>
-    </div>
-  )
+    return (
+        <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+            {/*"min-h-screen" will apply the background color defined in the index.html to the entier page*/}
+            <section className="min-h-screen flex flex-col">
+                {/*Rendering out the HFeader*/}
+                <Header />
+                {/*Logic for the page component rendering of the page (which one to show based on what data is available*/}
+                {/*Conditionally render when the output exists. The we render out the Information component. This is the first thing ot chek in the rendering of the page. Otherwise, we end up rendering the other component, even if the final output exist. That's why we check it first*/}
+                {/*If it does not exist we are going to check if loading exist. If it exists, we are going to render out Transcribing.*/}
+                {/*If also the Transcribing does not exist, we are going to finally renering the FileDisplay or the HomePage.*/}
+                {output ? (
+                    <Information />
+                ) : loading ? (
+                    <Transcribing />
+                ) : isAudioAvailable ? (
+                    // Passing down the file or the audioStream file as well as the handleAudioReset function.
+                    <FileDisplay file={file} audioStream={audioStream} handleAudioReset={handleAudioReset} />
+                ) : (
+                    // We are gonna pass the setFile and setAudioStream State variables down the Homepage component as a props.
+                    <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+                )}
+            </section>
+            <footer></footer>
+        </div>
+    )
 }
 
 export default App
--- react-js_free-scribe/src/components/Information.jsx
+++ react-js_free-scribe/src/components/Information.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Transcription from './Transcription';
+import Translation from './translation';
 
 export default function Information() {
+  // Define a State
+  const [tab, setTab] = useState('transcription');
+
   return (
-    <div>
-        
-    </div>
+    <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 justify-center text-center pb-20 max-w-prose w-full mx-auto">
+      <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
+
+      {/*Add the two button for the transcription and translation*/}
+      <div className='grid grid-cols-2 mx-auto bg-white shadow rounded-full overflow-hidden items-center'>
+        {/*Conditionally add some style to the button*/}
+        <button onClick={() => setTab('transcription')} className={'px-4 duration-200 py-1 font-medium' + (tab === 'transcription' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}>Transcription</button>
+        <button onClick={() => setTab('translation')} className={'px-4 duration-200 py-1 font-medium' + (tab === 'translation' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}>Translation</button>
+      </div>
+      {/*Conditionally render the transcription or translation component*/}
+      {tab === 'transcription' ? (
+        <Transcription />
+      ) : (
+        <Translation />
+      )}
+    </main>
   )
 }
