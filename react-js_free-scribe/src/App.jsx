4c2c48dc0c8867f8c38c0f80f58d694b9423b8c6
Alessandro Caula <alecolin96@gmail.com>
2024-11-09 16:39:59 +0100

    Main App.jsx logic for the Transcription of the audio recordings.

--- react-js_free-scribe/src/App.jsx
+++ react-js_free-scribe/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState, useRef, useEffect } from "react"
 import Header from "./components/Header"
 import FileDisplay from "./components/FileDisplay";
 import HomePage from "./components/Homepage";
 import Information from "./components/Information";
 import Transcribing from "./components/Transcribing";
+import { MessageTypes } from "./utils/presets";
 
 function App() {
     // useState variables
@@ -14,6 +15,9 @@ function App() {
     // The transcribed useState Hook variable final output
     const [output, setOutput] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [downloading, setDownloading] = useState(false);
+    // State variable for the transcription.
+    const [finished, setFinished] = useState(false);
 
     // Boolean check to see if there exist an audio available, both file or audioStream file.
     const isAudioAvailable = file || audioStream;
@@ -28,6 +32,70 @@ function App() {
     useEffect(() => {
         console.log(audioStream);
     }, [audioStream])
+
+    // Creating a worker reference. We will let the ML code for the transcription to work in the background.
+    const worker = useRef(null);
+
+    // This code sets up a Web Worker to handle background tasks for transcription without blocking the main UI
+    // The useEffect hook is used to run code after the component renders.
+    useEffect(() => {
+        // If the worker does not exists. Create it. 
+        if (!worker.current) {
+            // Creating a new Web Worker which will run the shisper.worker.js file in the utils folder. 
+            worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {
+                type: 'module'
+            })
+        }
+        // Logic to allow us to let the Web Worker and the main app to communicate.
+        // Function onMessageReceived to handle messages received from the worker.
+        const onMessageReceived = async (e) => {
+            switch (e.data.type) {
+                case 'DOWNLOADING':
+                    setDownloading(true);
+                    console.log('DOWNLOADING');
+                    break;
+                case 'LOADING':
+                    setLoading(true);
+                    console.log('LOADING');
+                    break;
+                case 'RESULT':
+                    setOutput(e.data.results);
+                    break;
+                case 'INFERENCE_DONE':
+                    setFinished(true);
+                    console.log('INFERENCE_DONE');
+                    break;
+            }
+        }
+        // This is the message event. 
+        worker.current.addEventListener('message', onMessageReceived)
+
+        // Cleanup function
+        return () => worker.current.removeEventListener('message', onMessageReceived)
+    });
+
+    // Function to get the audio buffer from the file or the transcription. 
+    async function readAudioFrom(file) {
+        const sampling_rate = 1600;
+        const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+        const response = await file.arrayBuffer();
+        const decoded = await audioCTX.decodeAudioData(response);
+        const audio = decoded.getChannelData(0);
+        return audio;
+    }
+
+    async function handleFormSubmission() {
+        if (!file && !audioStream) { return }
+
+        let audio = await readAudioFrom(file ? file : audioStream);
+        const model_name = `openai/whisper-tiny.en`
+
+        worker.current.postMessage({
+            type: MessageTypes.INFERENCE_REQUEST, 
+            audio, 
+            model_name
+        });
+    }
 
     return (
         <div className="flex flex-col max-w-[1000px] mx-auto w-full">
--- react-js_free-scribe/src/components/Header.jsx
+++ react-js_free-scribe/src/components/Header.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 export default function Header() {
     return (
         <header className="flex items-center justify-between gap-4 p-4">
-            <h1 className='font-semibold'>Free<span className="text-blue-400 bold">Scribe</span></h1>
+            {/*The <a> tage represents an anchor element, which is used to create hyperlinks.*/}
+            {/*These hyperlinks allow users to navigate to other web pages, jump to a different sectino of the same page, or initiate a download.*/}
+            {/*The href='/' creates a linkn that navigates to the root directory of the website.*/}
+            {/*This is done when the "New +" "button" is cilcked. Everything is reset. The page refresh.*/}
+            <a href='/'><h1 className='font-semibold'>Free<span className="text-blue-400 bold">Scribe</span></h1></a>
             {/*Givint the specialBtn class for styling this button*/}
-            <button className="flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400">
+            <a href='/' className="flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400">
                 <p>New</p>
                 <i className="fa-solid fa-plus"></i>
-            </button>
+            </a>
         </header>
     )
 }
