f8a900d782306a836e9c610a3bbed82dfe2dd10b
Alessandro Caula <alecolin96@gmail.com>
2024-11-27 00:53:30 +0100

    Subscription section completed.

--- nike_react-js/src/Sections/Subscribe.jsx
+++ nike_react-js/src/Sections/Subscribe.jsx
@@ -1,6 +1,24 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section id="contact-us" className="max-container flex justify-between items-center max-lg:flex-col gap-10 border">
+      <h3 className="font-palanquin font-bold text-3xl leading-[68px] lg:max-w-md border">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>
+        & Newsletter
+      </h3>
+      {/* max-sm: => apply the style to screen smaller than the sm breackpoint (640px by default) */}
+      {/* On large devices is going to take the 40% of the screen width */}
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full border">
+        {/* Input for the mail for the newsletter */}
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end max-sm:w-full border">
+          {/* fullWidth will be passed only then the div containing it is w-full */}
+          <Button label="Sign up" fullWidth />
+        </div>
+      </div>
+    </section>
   )
 }
 
--- nike_react-js/src/components/Button.jsx
+++ nike_react-js/src/components/Button.jsx
@@ -1,10 +1,11 @@
 // Destructuring the props that are arriving from the Hero component. 
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     // justify-center => center them horizonatlly 
     // items-center => center them vertically
     // Conditionally color the the button border, background and text colors of the button
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full  
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full 
+      ${fullWidth && `w-full`}
       ${backgroundColor ? backgroundColor : ` bg-coral-red `} 
       ${textColor ? textColor : ` text-white `}
       ${borderColor ? borderColor : ` border-coral-red`}`}
