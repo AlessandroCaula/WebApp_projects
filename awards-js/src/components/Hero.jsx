0b40017d55c1c8273c2d551169468153c1fb62e7
Alessandro Caula <alecolin96@gmail.com>
2024-12-01 23:51:58 +0100

    Implementation for the video mini player.

--- awards-js/src/components/Hero.jsx
+++ awards-js/src/components/Hero.jsx
@@ -1,14 +1,33 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const Hero = () => {
   // Define some useState variables that will say when a user has clicked something and we also have to keep track of which video is playing.
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
+  // Setting the isLoading state to true as initial state, caus usually a video takes some time to load.
+  const [isLoading, setIsLoading] = useState(true);
+  // Define another useState for the number of video that has loaded.
+  const [loadedCount, setLoadedCount] = useState(0);
+  
+  // Define the number of total video which you want to play (in this case we are set it to 4)
+  const totalVideo = 4;
+  // We also have to define the reference which will allow to then switch between those videos or to target the video player within which will play the videos.
+  // In react you use the useRef hook to create a reference to a DOM element.
+  // The DOM (Document Object Model) is a programming interface for web documents. It represents the structure of a web page as a tree of objects, where each element (like a <div>, <p>, or <img) is a node in the tree. This structure allows programming languages, like JavaScript, to dynamically interact with and manipulate the content, structure, and styling of a webpage.
+  // useRef is a React hook used to create a mutable reference object that persists across renders. It provides access to a .current property where you can store a value that doesn't trigger a re-render when it changes.
+  // We are going to use it to target the <div> within which we want to play the videos.
+  const nextVideoRef = useRef(null);
 
   // Define some new functions that will handle the mini video player. This video player will show different videos when clicking in the middle of the screen.
-  const handleMiniVdClick = () => {
-
+  const handleMiniVideoClick = () => {
+    // When the mini-player is clicked we want to set the hasClicked state to true.
+    setHasClicked(true);
+    // We also want to set the currentIndex state to be equal to a function where we get the previous index and add 1 to it.
+    setCurrentIndex((prevIndex) => prevIndex + 1);
   }
+
+  // Define the source of the videos to be played. As a function that will return the source of the videos, with the index in their name. Givin the path of each video source.
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     // h-dvh => Sets the height of an element to the dynamic viewport height (dvh), which is a CSS unit introduced to handle viewport height changes, especially on mobile devices with UI overlays. 
@@ -21,8 +40,11 @@ const Hero = () => {
           {/* mask-clip-path => our special css class name */}
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             {/* Creating another div, which will actually be a mini video player. */}
-            <div>
-              MiniVideoPlayer
+            <div onClick={handleMiniVideoClick} className="origin-center">
+              {/* Implement the video player as a HTML self-closing video tag */}
+              <video 
+                ref={nextVideoRef}
+              />
             </div>
           </div>
         </div>
