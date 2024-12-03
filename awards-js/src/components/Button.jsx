8090207be26461bbdc0705d7bd543e906924d0e0
Alessandro Caula <alecolin96@gmail.com>
2024-12-03 21:13:34 +0100

    New custom Button developed.

--- awards-js/src/components/Button.jsx
+++ awards-js/src/components/Button.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
-const Button = () => {
+// Deconstructing all the properties that are passed to this Button component.
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
-    <div>Button</div>
+    // Create out custom button inside a button tag.
+    // Make the className dynamic (transforming it to a dynamic template string), so that we can add the containerClass classes to the button, in order in this way to be able to style it from the outside.
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+      {/* Rendering the leftIcon */}
+      {leftIcon}
+      {/* Creating a span in this button */}
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>
+          {title}
+        </div>
+      </span>
+      {/* Rendering the Right Icon */}
+    {rightIcon}
+    </button>
   )
 }
 
