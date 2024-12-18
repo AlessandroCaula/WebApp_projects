e2b1adef2d12c59ba500bdca008438ea1648cf8e
Alessandro Caula <alecolin96@gmail.com>
2024-12-18 18:15:27 +0100

    New changes on the Story component

--- awwwards-js/src/components/Story.jsx
+++ awwwards-js/src/components/Story.jsx
@@ -1,5 +1,7 @@
 import { useRef } from "react"
 import AnimatedTitle from "./AnimatedTitle"
+import gsap from "gsap";
+import RoundedCorners from "./RoundedCorners";
 
 const Story = () => {
 
@@ -9,7 +11,46 @@ const Story = () => {
 
   // Define a new function that will handle the events when the mouse leave. But in our case also when it enters the image.
   const handleMouseLeave = () => {
-    
+    // Get the element, the image Ref, on which the mouse is hovering. 
+    const element = frameRef.current;
+    // Now if we are gonna leave the element, we are going to reset the positioning. 
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0, 
+      rotateY: 0,
+      ease: 'power1.inOut'
+    });
+  }
+  // Define a function that will handle the mouse move event, when the mouse is hovering and moving on the <img> DOM element.
+  const handleMouseMove = (e) => {
+    // Destructure the client X and Y position. 
+    const { clientX, clientY } = e;
+    // Get the element, the image Ref, on which the mouse is hovering. 
+    const element = frameRef.current;
+    // If there is no element, we are gonna simply exit from the function. 
+    if (!element) return;
+
+    // If we do have it, we are going to take its rectangular properties.
+    const rect = element.getBoundingClientRect();
+    // Now we can extract its X position and Y position. 
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    // Calculate the center of the rectangle client. 
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    // Finally we can calculate the rotation values based on the mouse position relative to the center.
+    const rotateX = ((y - centerY) / centerY) * -10; // -10 => This is used as a magnifier.
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    // Finally we can use gsap to animate it, passing the element and the properties for the animation. 
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX, rotateY,
+      transformPerspective: 500, // This is gonna give a 3D effect
+      ease: 'power1.inOut'
+    });
   }
 
   return (
@@ -24,7 +65,7 @@ const Story = () => {
         </p>
         {/* Create the animated title */}
         <div className='relative size-full'>
-          <AnimatedTitle 
+          <AnimatedTitle
             title="The st<b>o</b>ry of <br /> a hidden real<b>m</b>"
             // Give a section ID so we can scroll to it. 
             sectionId="#story"
@@ -34,9 +75,15 @@ const Story = () => {
           <div className="story-img-container">
             <div className="story-img-mask">
               <div className="story-img-content">
-                <img 
+                <img
                   // Give to the img the ref, so that we will be able to animate if on mouse hover.
                   ref={frameRef}
+                  // We are gonna call the same handleMouseLeave function for entering, leaving and up the mouse from the image. 
+                  onMouseLeave={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  // The only one that will be different, will be onMouseMove
+                  onMouseMove={handleMouseMove}
                   src="/img/entrance.webp"
                   // The alt tag is an attribute used in the <img> HTML tah to provide a textual description of an image. 
                   // 1. The alt text is read by screen readers to describe the image for visually impaired users. 
@@ -46,6 +93,9 @@ const Story = () => {
                 />
               </div>
             </div>
+
+            <RoundedCorners />
+            
           </div>
         </div>
       </div>
