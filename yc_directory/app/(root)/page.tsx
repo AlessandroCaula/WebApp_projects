19b84d4aed9048d694bcec87059d74b23d3b9779
Alessandro Caula <alecolin96@gmail.com>
2025-01-11 16:40:52 +0100

    Hero section initial implementation.

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      {/* The pink_container is defined in the global.css file. */}
+      <section className="pink_container">
+        {/* The heading class is defined in the global.css file as a custom style class */}
+        <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+
+        {/* !max-w-3xl => the ! will force the max-w-3xl to override what it is the style in the sub-heading class of the global.css */}
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+
+      </section>
     </>
   );
 }
