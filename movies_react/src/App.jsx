9dcce932acb19313f69895162a9f62cb80785d8a
AlessandroCaula <alecaula96@gmail.com>
2025-02-13 00:43:29 +0100

    Fetching the data from the movie DataBase.

--- movies_react/src/App.jsx
+++ movies_react/src/App.jsx
@@ -19,35 +19,68 @@ const API_OPTIONS = {
   }
 };
 
-// Defining the 
 
 // Main App function component.
 const App = () => {
 
   // Create the Search film component
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
+  // In order to display any fetching error in the browser, define a new useState variable
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Defining the function used to fetch those movies. Which will an async function. 
+  const fetchMovies = async () => {
+    // Open a try catch block if something fails when fetching the data 
+    try {
+      // Define the exact endpoint that we are trying to call. 
+      // That will be equal to a template string, where we put together the API base URL 
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      // Once we have the endpoint we can try to call it.
+      // The fetch() is a built in JavaScript function that allows you to make HTTP requests, like get or post to different apis or servers.
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      // // To check if the data has been correctly fetched. 
+      // alert(response);
+      // // Simulating an wrong call while fetching the data by throwing an error
+      // throw new Error('Failed to fetch the movies');
+
+    } catch (error) {
+      console.log(`Error fetching movies ${error}`);
+      // If we catch any error, set the errorMessage state variable
+      setErrorMessage("Error fetching data. Please try again later.");
+    }
+  }
 
   // Create a useEffect hook for fetching the data. That will only load at the start, by using the empty dependency array [].
   useEffect(() => {
-
+    // Call the fetch movies function to fetch the data as first load of the application
+    fetchMovies();
   }, [])
 
   return (
     <main>
-      <div className='pattern'/>
+      <div className='pattern' />
 
       {/* Create the header */}
       <div className='wrapper'>
         <header>
           {/* Rendering the hero-img */}
-          <img src='./hero-img.png' alt='Hero Banner'/>
+          <img src='./hero-img.png' alt='Hero Banner' />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          {/* Passing the search term and setSearchTerm to the Search component */}
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          {/* To check that while typing the searchTerm is being updated
+          <h1 className='text-white'>{searchTerm}</h1> */}
         </header>
-        
-        {/* Passing the search term and setSearchTerm to the Search component */}
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        {/* To check that while typing the searchTerm is being updated
-        <h1 className='text-white'>{searchTerm}</h1> */}
+
+        {/* Section about movies */}
+        <section className='all-movies'>
+          <h2>All Movies</h2>
+
+          {/* If there is an error message then render a <p> tag. Using dynamic rendering */}
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+        </section>
       </div>
     </main>
   )
