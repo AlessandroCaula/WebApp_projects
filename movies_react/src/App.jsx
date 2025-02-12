5c19b3eb0792894a60caf97bd4152955aec03b90
AlessandroCaula <alecaula96@gmail.com>
2025-02-12 23:52:26 +0100

    API request

--- movies_react/src/App.jsx
+++ movies_react/src/App.jsx
@@ -1,10 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './components/Search'
 
+// API - Application Programming Interface:
+//  - A set of rules that allows one software application to talk to another.
+// The following is the URL of the TMDB database for requesting the most popular movies.
+const API_BASE_URL = "https://api.themoviedb.org/3";
+// Get access to the API key. 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// Defining the API options. 
+const API_OPTIONS = {
+  // Defining the method as 'GET', as we want to get the movies.
+  method: 'GET',
+  header: {
+    // We can define what kind of data we accept in our application. 
+    accept: 'application/json', // Which means that the API will send back a json object similar to a basic JavaScript object 
+    // Authenticate the API. That will verify who is trying to make the request
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+// Defining the 
+
+// Main App function component.
 const App = () => {
 
   // Create the Search film component
   const [searchTerm, setSearchTerm] = useState("");
+
+  // Create a useEffect hook for fetching the data. That will only load at the start, by using the empty dependency array [].
+  useEffect(() => {
+
+  }, [])
 
   return (
     <main>
